ee08b5b62db019a18ed0f59afe1c89898e03a543
hanhngooo <hanhngo1795@gmail.com>
2020-08-10 14:58:14 +0200

    display Popup with info on click the circle

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
-// import numeral from "numeral";
-import { Circle } from "react-leaflet";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
 import "./Map.css";
 
 function Map(props) {
@@ -38,7 +38,28 @@ function Map(props) {
                 Math.sqrt(country[props.casesType]) *
                 casesTypeColors[props.casesType].multiplier
               }
-            ></Circle>
+            >
+              <Popup>
+                <div className="info-container">
+                  <div
+                    className="info-flag"
+                    style={{
+                      backgroundImage: `url(${country.countryInfo.flag})`,
+                    }}
+                  ></div>
+                  <div className="info-name">{country.country}</div>
+                  <div className="info-confirmed">
+                    Cases: {numeral(country.cases).format("0,0")}
+                  </div>
+                  <div className="info-recovered">
+                    Recovered: {numeral(country.recovered).format("0,0")}
+                  </div>
+                  <div className="info-deaths">
+                    Deaths: {numeral(country.deaths).format("0,0")}
+                  </div>
+                </div>
+              </Popup>
+            </Circle>
           ))}
       </LeafletMap>
     </div>
