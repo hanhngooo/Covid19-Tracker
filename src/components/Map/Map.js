56907e5e2602d0b3d5e72d0d01d7d41d7b860648
hanhngooo <hanhngo1795@gmail.com>
2020-08-10 14:44:44 +0200

    displaying circles on the map using Circle from react-leaflet

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,17 +1,45 @@
 import React from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
-
+// import numeral from "numeral";
+import { Circle } from "react-leaflet";
 import "./Map.css";
 
 function Map(props) {
+  const casesTypeColors = {
+    cases: {
+      hex: "#CC1034",
+      multiplier: 800,
+    },
+    recovered: {
+      hex: "#7dd71d",
+      multiplier: 1200,
+    },
+    deaths: {
+      hex: "#fb4443",
+      multiplier: 2000,
+    },
+  };
+
   return (
     <div className="map">
-      {props.iso}
       <LeafletMap center={props.center} zoom={props.zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        {props.countries &&
+          props.countries.map((country) => (
+            <Circle
+              center={[country.countryInfo.lat, country.countryInfo.long]}
+              color={casesTypeColors[props.casesType].hex}
+              fillColor={casesTypeColors[props.casesType].hex}
+              fillOpacity={0.4}
+              radius={
+                Math.sqrt(country[props.casesType]) *
+                casesTypeColors[props.casesType].multiplier
+              }
+            ></Circle>
+          ))}
       </LeafletMap>
     </div>
   );
